70e0b99fa3d9a2351137eb1ca65bc0cf9aecc93c
pittalaabhishek <pittalaabhishek999@gmail.com>
2025-04-23 12:49:09 +0530

    Add product and review controller logic

--- models/review.go
+++ models/review.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Review struct {
 	gorm.Model
-	ProductID uint   `json:"product_id"`
-	Name      string `json:"name"`
+	ProductID uint   `json:"product_id" gorm:"not null"`
+	Name      string `json:"name" gorm:"default:'Anonymous'"`
 	Review    string `json:"review"`
 	Rating    int    `json:"rating" gorm:"check:rating >= 1 AND rating <= 5"`
 }
